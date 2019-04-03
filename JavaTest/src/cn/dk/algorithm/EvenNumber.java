13d28653e1494ff71a14396415ccae0499335453
DeathKnightH <919438785@qq>
2019-04-03 16:08:47 +0800

    add isEvenNumberByte method: judge a Integer is even by bit operation

--- JavaTest/src/cn/dk/algorithm/EvenNumber.java
+++ JavaTest/src/cn/dk/algorithm/EvenNumber.java
@@ -2,10 +2,16 @@ package cn.dk.algorithm;
 
 public class EvenNumber {
     public static void main(String[] args) {
-        isEvenNumber(3);
+        int x = 4;
+        boolean flag = isEvenNumber(x);
+        boolean flagByte = isEvenNumberByte(x);
+        System.out.println(flag);
+        System.out.println(flagByte);
     }
 
     public static boolean isEvenNumber(int x){
         return x%2 == 0;
     }
+
+    public static boolean isEvenNumberByte(int x) { return (x & 0x00000001) == 0;}
 }
