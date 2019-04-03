60fab974db41ce5761844bf22f1be46c25d78af2
DeathKnightH <919438785@qq>
2019-04-03 13:55:04 +0800

    add function: isEvenNumber

---
+++ JavaTest/src/cn/dk/algorithm/EvenNumber.java
@@ -0,0 +1,11 @@
+package cn.dk.algorithm;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+        isEvenNumber(3);
+    }
+
+    public static boolean isEvenNumber(int x){
+        return x%2 == 0;
+    }
+}
