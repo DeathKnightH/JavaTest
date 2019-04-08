695c4e30ecb42438d33bfde2f41f7532fa630bff
DeathKnightH <919438785@qq>
2019-04-08 15:23:52 +0800

    add function:romanToInteger

---
+++ JavaTest/src/cn/dk/algorithm/leetcode/RomanToInteger.java
@@ -0,0 +1,46 @@
+package cn.dk.algorithm.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        RomanToInteger romanToInteger = new RomanToInteger();
+        int number = romanToInteger.romanToInteger("IV");
+        System.out.println(number);
+    }
+
+    public int romanToInteger(String roman){
+        char[] charList = roman.toCharArray();
+        List<Integer> numberList = new ArrayList<>();
+        for (char temp: charList ) {
+            if (temp == 'I'){
+                numberList.add(1);
+            }else if (temp == 'V'){
+                numberList.add(5);
+            }else if (temp == 'X'){
+                numberList.add(10);
+            }else if (temp == 'L'){
+                numberList.add(50);
+            }else if (temp == 'C'){
+                numberList.add(100);
+            }else if (temp == 'D'){
+                numberList.add(500);
+            }else if (temp == 'M'){
+                numberList.add(1000);
+            }
+        }
+        int flag = 4000;
+        int result = 0;
+        for (int i = 0; i < numberList.size(); i++) {
+            int temp = numberList.get(i);
+            if (flag < temp){
+                result = result + temp - 2 * flag;
+            }else{
+                result += temp;
+            }
+            flag = temp;
+        }
+        return result;
+    }
+}
