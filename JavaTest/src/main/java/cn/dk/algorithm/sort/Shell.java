8e16b2e3b5f7f7634fe7a5bd1a4800a1def3924e
DeathKnightH <919438785@qq>
2019-07-10 17:36:13 +0800

    add comment to shell-sort

--- JavaTest/src/main/java/cn/dk/algorithm/sort/Shell.java
+++ JavaTest/src/main/java/cn/dk/algorithm/sort/Shell.java
@@ -10,12 +10,12 @@ public class Shell {
         int sumTimes = 0;
         int increment = intArray.length;
         do {
-            increment = increment/3 + 1;
-            for (int i = increment; i < intArray.length; i++) {
-                if(intArray[i] < intArray[i - increment]){
+            increment = increment/3 + 1;        // 增量步长设为n/3 + 1，也有设置为 n/2的，具体没有固定的公式和结论
+            for (int i = increment; i < intArray.length; i++) { // 从下标为 increment处开始循环，因为前increment个元素在内部循环中会比较到
+                if(intArray[i] < intArray[i - increment]){      // 因为把整个数组分为了 increment 个子数组进行插入排序，所以比较和递增的步长为 increment
                     int temp = intArray[i];
                     int j;
-                    for (j = i - increment; j >= 0 && intArray[j] > temp ; j -= increment) {
+                    for (j = i - increment; j >= 0 && intArray[j] > temp ; j -= increment) {    //向前比较，直到找到此元素在子数组的位置，将其插入
                         intArray[j + increment] = intArray[j];
                         sumTimes ++;
                     }
