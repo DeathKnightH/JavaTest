16f09c77e47f5e314be43d337d4010c77dc80310
DeathKnightH <919438785@qq>
2020-03-26 17:27:40 +0800

    fix partition function

--- src/main/java/cn/dk/algorithm/sort/Quick.java
+++ src/main/java/cn/dk/algorithm/sort/Quick.java
@@ -1,5 +1,7 @@
 package cn.dk.algorithm.sort;
 
+import java.util.Arrays;
+
 /**
  * quick sort
  */
@@ -13,7 +15,7 @@ public class Quick {
      * @return
      */
     public static int[] quickSort(int[] origin, int start, int end) {
-        if (origin == null || origin.length == 0 || start < 0 || end > origin.length - 1 || start > end) {
+        if (origin == null || origin.length == 0 || start < 0 || end > origin.length - 1 || start >= end) {
             return null;
         }
         int partition = partition(origin, start, end);
@@ -27,21 +29,40 @@ public class Quick {
     }
 
     private static int partition(int[] origin, int start, int end) {
+        if (end - start == 1) {
+            if (origin[start] > origin[end]) {
+                swapArray(origin, start, end);
+                return start;
+            }
+        }
         int stand = origin[start];
-        int i = start;
-        for (i = start + 1; i <= end; i++) {
+        for (int i = start + 1; i <= end; i++) {
             if (stand <= origin[i]) {
                 swapArray(origin, i, end);
                 end--;
             }
         }
-        swapArray(origin, start, i);
-        return i;
+        if (origin[start] > origin[end]) {
+            swapArray(origin, start, end);
+            return end;
+        } else {
+            swapArray(origin, start, end - 1);
+            return end - 1;
+        }
     }
 
     private static void swapArray(int[] origin, int x, int y) {
         int temp = origin[x];
         origin[x] = origin[y];
-        origin[y] = origin[temp];
+        origin[y] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] testArray = new int[]{5, 4, 6, 8, 7, 1};
+        int partition = partition(testArray, 0, 5);
+        System.out.println("Partition index: " + partition + ". Array: " + Arrays.toString(testArray));
+        testArray = new int[]{3, 2, 1};
+        partition = partition(testArray, 0, 2);
+        System.out.println("Partition index: " + partition + ". Array: " + Arrays.toString(testArray));
     }
 }
--- src/test/java/cn/dk/algorithm/sort/QuickTest.java
+++ src/test/java/cn/dk/algorithm/sort/QuickTest.java
@@ -12,16 +12,16 @@ public class QuickTest {
 
         System.out.println(Arrays.toString(case1));
 
-        int[] case2 = ArrayToBeSort.generateNewArrayToBeSortRandom(100000);
-        long start = System.currentTimeMillis();
-        Quick.quickSort(case2, 0, case2.length - 1);
-        long end = System.currentTimeMillis();
-        System.out.println("10w耗时" + (end - start));
+//        int[] case2 = ArrayToBeSort.generateNewArrayToBeSortRandom(100000);
+//        long start = System.currentTimeMillis();
+//        Quick.quickSort(case2, 0, case2.length - 1);
+//        long end = System.currentTimeMillis();
+//        System.out.println("10w耗时" + (end - start));
 
-        int[] case3 = ArrayToBeSort.generateNewArrayToBeSortRandom(10000000);
-        start = System.currentTimeMillis();
-        Quick.quickSort(case3, 0, case3.length - 1);
-        end = System.currentTimeMillis();
-        System.out.println("1000w耗时" + (end - start));
+//        int[] case3 = ArrayToBeSort.generateNewArrayToBeSortRandom(10000000);
+//        start = System.currentTimeMillis();
+//        Quick.quickSort(case3, 0, case3.length - 1);
+//        end = System.currentTimeMillis();
+//        System.out.println("1000w耗时" + (end - start));
     }
 }
